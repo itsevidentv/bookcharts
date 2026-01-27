853dc1761302ee5a36f77a88f50ce1b6cb99e68a
tookthat <apocalypseanytime@gmail.com>
2026-01-27 15:31:36 +0800

    2026 books

---
+++ markers/markers-2026.js
@@ -0,0 +1,15 @@
+const markers2026 = [
+{name:"Persuasion",position:{top:"46%",left:"65%"},color:"fic"},
+{name:"The Little Sparrow Murders",position:{top:"90%",left:"51%"},color:"fic"},
+{name:"Bright Sorrow",position:{top:"27%",left:"67%"},color:"other"},
+{name:"8 Lives of a Century-Old Trickster",position:{top:"47%",left:"43%"},color:"fic"},
+{name:"Seventeen",position:{top:"56%",left:"38%"},color:"fic"},
+{name:"The House of Doors",position:{top:"36%",left:"73%"},color:"fic"},
+{name:"Cowboy Graves",position:{top:"40%",left:"76%"},color:"fic"},
+{name:"Helm",position:{top:"37%",left:"74%"},color:"fic"},
+{name:"Hunger",position:{top:"26%",left:"63%"},color:"fic"},
+{name:"Death on Gokumon Island",position:{top:"88%",left:"60%"},color:"fic"},
+{name:"The Vegetarian",position:{top:"22%",left:"70%"},color:"fic"},
+{name:"If We Were Villains",position:{top:"56%",left:"65%"},color:"fic"},
+{name:"Authority", position:{top:"35%",left:"80%"}, color:"fic"}
+];
