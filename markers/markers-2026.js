5d061dffb28c75307c41893385d658bf96761088
itsevidentv <spammedalot@proton.me>
2026-01-28 21:05:50 +0800

    Update markers-2026.js

--- markers/markers-2026.js
+++ markers/markers-2026.js
@@ -1,15 +1,4 @@
 const markers2026 = [
-{name:"Persuasion",position:{top:"46%",left:"65%"},color:"fic"},
-{name:"The Little Sparrow Murders",position:{top:"90%",left:"51%"},color:"fic"},
-{name:"Bright Sorrow",position:{top:"27%",left:"67%"},color:"poetry"},
-{name:"8 Lives of a Century-Old Trickster",position:{top:"52%",left:"43%"},color:"fic"},
-{name:"Seventeen",position:{top:"56%",left:"38%"},color:"fic"},
-{name:"The House of Doors",position:{top:"36%",left:"73%"},color:"fic"},
-{name:"Cowboy Graves",position:{top:"40%",left:"76%"},color:"fic"},
-{name:"Helm",position:{top:"37%",left:"74%"},color:"fic"},
-{name:"Hunger",position:{top:"26%",left:"63%"},color:"fic"},
-{name:"Death on Gokumon Island",position:{top:"88%",left:"56%"},color:"fic"},
-{name:"The Vegetarian",position:{top:"22%",left:"70%"},color:"fic"},
-{name:"If We Were Villains",position:{top:"56%",left:"60%"},color:"fic"},
-{name:"Authority", position:{top:"35%",left:"80%"}, color:"fic"}
+{name:"Persuasion", position:{top:"46%", left:"65%"}, color:"fic"},
+{name:"Authority", position:{top:"35%", left:"80%"}, color:"fic"}
 ];
